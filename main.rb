bc928ece0123640b0774bf21c00ae5701e242f83
Ricardo López Salazar <Rash.Pro@gmail.com>
2012-11-29 18:10:22 -0600

    re arrenged the directory layout

--- main.rb
+++ main.rb
@@ -10,6 +10,8 @@ class Game < Chingu::Window
     super(800,600,false)              # leave it blank and it will be 800,600,non fullscreen
     self.input = { :escape => :exit } # exits example on Escape
     
+    @bgmusic = Song["come-together.ogg"]
+    @bgmusic.play
     @player = Player.create(:x => 200, :y => 200, :image => Image["player.png"])
     @player.input = { :holding_left => :move_left, :holding_right => :move_right, 
                       :holding_up => :move_up, :holding_down => :move_down }
